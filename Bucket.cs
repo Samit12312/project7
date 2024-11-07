ec4ba2d28e5d75a23e616d3a334fe4dadb12cfac
Owner <Owner@660084-COMPN-09>
2024-11-07 10:17:49 +0200

    Add project files.

---
+++ Bucket.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project7
+{
+    internal class Bucket
+    {
+        int capacity;
+        int amount;
+        public Bucket(int capacity)
+        {
+            this.capacity = capacity;
+        }
+        void Empty()
+        {
+            this.amount = 0;
+        }
+        void Fill() 
+        {
+            this.amount = capacity;
+        }
+        void PourIntro(Bucket bucket) 
+        {
+           if (bucket.capacity - bucket.amount > this.amount)
+            {
+                bucket.amount += this.amount;
+                this.amount = 0;
+            }
+           else
+            {
+                int am = bucket.capacity - bucket.amount;
+                bucket.amount += am;
+                this.amount -= am;
+            }
+        }
+        public override string ToString() 
+        {
+            string b = $"capacity = {this.capacity} amount = {this.amount}";
+            return b;
+            
+        }
+
+    }
+}
